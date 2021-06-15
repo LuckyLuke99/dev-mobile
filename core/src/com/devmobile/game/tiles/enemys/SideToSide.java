9c59bfc9f1d9440e5ed54d5089617916a0b5c0f4
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2021-06-15 17:37:44 -0300

    Começando a fazer os inimigos

--- core/src/com/devmobile/game/tiles/enemys/SideToSide.java
+++ core/src/com/devmobile/game/tiles/enemys/SideToSide.java
@@ -4,21 +4,24 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.devmobile.game.helpers.GameInfo;
 
+import java.awt.Rectangle;
+
 public class SideToSide {
-    private int maxRange, pos;
+    private Rectangle rectangle;
+    private float maxRange;
     protected float acel, vel;
 
-    public SideToSide(float acel, int range, float position){
-        maxRange = range * GameInfo.sizeTexture;
-        pos = position;
-        this.acel = acel;
+    public SideToSide(Rectangle rectangle, int range, float acel){
+//        this.rectangle = rectangle;
+//        this.acel = acel;
+//        maxRange = rectangle.getX() + (range * GameInfo.sizeTexture);
     }
 
     private void movement (){
         float deltaTime = Gdx.graphics.getDeltaTime();
-        if(pos < maxRange){
-
-        }
+//        if(pos < maxRange){
+//
+//        }
         vel += acel * deltaTime;
 
     }
