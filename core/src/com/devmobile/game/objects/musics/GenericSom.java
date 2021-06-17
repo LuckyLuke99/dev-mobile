ae3d2ab34c5a5d99c13f2731f2b0f56eec80b14d
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2021-06-17 19:04:25 -0300

    Adicionando som de fundo e das moedas

--- core/src/com/devmobile/game/objects/musics/GenericSom.java
+++ core/src/com/devmobile/game/objects/musics/GenericSom.java
@@ -1,7 +1,6 @@
 package com.devmobile.game.objects.musics;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.devmobile.game.helpers.GameInfo;
 
